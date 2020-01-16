527422b1a0c9ed31c9775678b826becb0ac56693
Darren Paul <drobertpaul@gmail.com>
2020-01-16 23:13:11 +0000

    added task to add to db

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
+import time
 from modules import collector
 from modules.mongo_manager import mongo_man
 
+INTERVALS = 3600
 currency_pair = 'btceur'
 
-# get latest price
-latest_price = collector.latest_ticker(currency_pair)
-print(latest_price)
-# add latest price to database
-db = mongo_man.Mongo('crypto', currency_pair)
-db.add_single(latest_price)
-# get latest 10 entries in database
+while True:
+    # get latest price
+    latest_price = collector.latest_ticker(currency_pair)
+    print(latest_price)
+    # add latest price to database
+    db = mongo_man.Mongo('crypto', currency_pair)
+    db.add_single(latest_price)
+    # get latest 10 entries in database
 
-# get average of last price from database entries
+    # get average of last price from database entries
 
-# check if can sell
+    # check if can sell
 
-# check if can buy
+    # check if can buy
+    time.sleep(self.intervals)
